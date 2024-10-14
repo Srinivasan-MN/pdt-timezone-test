aa54a341d9f9a3f19d105bcedb51c3a8357e3062
Srinivasan <srinivasan20012018@gmail.com>
2024-10-14 15:07:46 +0530

    removed req.txt block

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("PDT time zone test...")
+
+            # - name: install python packages
+            #   run: 
+            #     python -m pip install --upgrade pip
+            #     pip install -r requirements.txt
