35753fd1d2b80375cdc65e67f2bd3628c96b6c63
Srinivasan <srinivasan20012018@gmail.com>
2024-10-15 15:03:47 +0530

    changed the time 6

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-print("PDT time zone test...")
+import yaml
 
-            # - name: install python packages
-            #   run: 
-            #     python -m pip install --upgrade pip
-            #     pip install -r requirements.txt
+print("PDT time zone test...")
+class CustomLoader(yaml.SafeLoader):
+    pass
+def construct_yaml_str(self, node):
+    return self.construct_scalar(node)
+
+CustomLoader.add_constructor('tag:yaml.org,2002:bool', construct_yaml_str)
+
+class CustomDumper(yaml.SafeDumper):
+    pass
+
+def str_presenter(dumper, data):
+    # If the string data is 'on', return it unquoted
+    if data in ['on', 'yes', 'no', 'true', 'false']:
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+CustomDumper.add_representer(str, str_presenter)
+
+
+def convert_pdt_to_pst():
+    with open('.github/workflows/pdt-tz-test.yml', 'r') as file:
+        # yamlData = yaml.safe_load(file)
+        # yamlData = yaml.load(stream=file, Loader=yaml.BaseLoader)
+        yamlData = yaml.load(file, Loader=CustomLoader)
+    
+    print(yamlData)
+        
+    with open('.github/workflows/pdt-tz-test1.yml', 'w') as file:
+        yaml.dump(yamlData, file,Dumper=CustomDumper,default_flow_style=False)
+    
+convert_pdt_to_pst()    
